a2baa53d5d4ee0efb15f9ad8590744fd2d5b1ca5
ebots-hung <hlam@ebots.com>
2021-06-16 18:40:15 -0700

    update input/output video arguments

--- AdvLaneline_main.py
+++ AdvLaneline_main.py
@@ -6,7 +6,7 @@
 
 import os
 import cv2
-import glob
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import settings
@@ -20,12 +20,18 @@ dump_image_folderpath = "output_images"
 test_image_folderpath = "test_images"
 
 if __name__ == '__main__':
-    currentfolderpath = os.path.abspath(os.getcwd())
-    filetoread = os.path.join(currentfolderpath, dump_image_folderpath, "camcalibration.ini")
-    settings.read_calibration_file(filetoread)
-    imgcopy = cv2.imread(os.path.join(currentfolderpath, test_image_folderpath, "test1.jpg"))
-    settings.init(imgcopy)
-    video_input1 = VideoFileClip('project_video.mp4')#.subclip(22,26)
-    video_output1 = 'project_video_output.mp4'
-    processed_video = video_input1.fl_image(process_image)
-    processed_video.write_videofile(video_output1, audio=False)
+    # total arguments
+    n = len(sys.argv)
+    print("Total arguments passed:", n)
+    if n == 3: 
+        input_video = sys.argv[1]
+        output_video = sys.argv[2]
+        currentfolderpath = os.path.abspath(os.getcwd())
+        filetoread = os.path.join(currentfolderpath, dump_image_folderpath, "camcalibration.ini")
+        settings.read_calibration_file(filetoread)
+        imgcopy = cv2.imread(os.path.join(currentfolderpath, test_image_folderpath, "test1.jpg"))
+        settings.init(imgcopy)
+        video_input1 = VideoFileClip(input_video)#.subclip(22,26)
+        # video_output1 = 'project_video_output.mp4'
+        processed_video = video_input1.fl_image(process_image)
+        processed_video.write_videofile(output_video, audio=False)
